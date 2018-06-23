9eceac374e3f70d4af87b0d76b9aea5b8df9fe34
zhengyingdeng <892467242@qq.com>
2018-06-23 22:28:58 +0800

    第二章

---
+++ chapter2-1/src/main/java/com/didispace/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.didispace.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZhengYingDeng
+ * @email 892467242@qq.com
+ * @create 19:25 2018/6/22
+ **/
+@RestController
+public class HelloController {
+
+
+    public String index() {
+        return "Hello World";
+    }
+}
