7e6949d02c205ccf09610e764dc781b52e1fde3b
zhengyingdeng <892467242@qq.com>
2018-06-25 18:07:07 +0800

    第二章

---
+++ chapter2-1/src/main/java/com/didispace/domain/Book.java
@@ -0,0 +1,46 @@
+package com.didispace.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ZhengYingDeng
+ * @email 892467242@qq.com
+ * @create 16:17 2018/6/25
+ **/
+@Component
+public class Book {
+
+    @Value("${book.name}")
+    private String name;
+
+    @Value("${book.author}")
+    private String author;
+
+    @Value("${book.desc}")
+    private String desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
--- chapter2-1/src/test/java/com/didispace/ApplicationTests.java
+++ chapter2-1/src/test/java/com/didispace/ApplicationTests.java
@@ -1,10 +1,22 @@
 package com.didispace;
 
+import com.didispace.domain.Book;
+import com.didispace.web.HelloController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -12,9 +24,24 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class ApplicationTests {
 
+	private MockMvc mvc;
+
+	@Autowired(required = false)
+	private Book book;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
 	@Test
 	public void test() throws Exception {
-
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
+		System.out.println(book.getName());
+		System.out.println(book.getAuthor());
+		System.out.println(book.getDesc());
 	}
 
 }
