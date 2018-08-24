3fab3b24e8f14c7b4d3da92efd242e1634a34856
zhengyingdeng <892467242@qq.com>
2018-08-24 18:34:26 +0800

    上传新代码

--- c-api-gateway/src/main/java/com/ad/capigateway/CApiGatewayApplication.java
+++ c-api-gateway/src/main/java/com/ad/capigateway/CApiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.ad.capigateway;
 import com.ad.capigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class CApiGatewayApplication {
 
--- eureka-provider/src/main/java/com/ad/eurekaprovider/EurekaProviderApplication.java
+++ eureka-provider/src/main/java/com/ad/eurekaprovider/EurekaProviderApplication.java
@@ -3,10 +3,24 @@ package com.ad.eurekaprovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaProviderApplication {
+
+	@RequestMapping(value = "/hello")
+	public String index() {
+		System.out.println("request is coming...");
+		try {
+			Thread.sleep(100000);
+		} catch (InterruptedException e) {
+			System.out.println("线程被打断... " + e.getMessage());
+		}
+		return "hello spring ...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaProviderApplication.class, args);
